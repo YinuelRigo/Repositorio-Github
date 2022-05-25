d530c2e93c026bac7b6ecdb0ca330c5e8d06ceae
YinuelRigo <shadicer31@gmail.com>
2022-05-25 18:12:18 -0500

    Agregar archivos de proyecto.

---
+++ Laberinto/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace Laberinto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Solve";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(174, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(281, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Path";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(255, 15);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Black;
+            this.pictureBox2.Location = new System.Drawing.Point(316, 15);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(341, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Wall";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(93, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Hilos(2)";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 313);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Laberinto/Form1.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Laberinto
+{
+    public partial class Form1 : Form
+    {
+        Color currentColor = Color.White;
+        private int XTILES = 25; //Numero de pixeles en x
+        private int YTILES = 25; //Numero de pixeles en y
+        private int TILESIZE = 10; //Tamaño de pixeles
+        private PictureBox[,] mazeTiles;
+        private bool hilos = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            createNewMaze();
+        }
+
+        private void createNewMaze()
+        {
+            mazeTiles = new PictureBox[XTILES, YTILES];
+
+            for (int i = 0; i < XTILES; i++)
+            {
+                for (int j = 0; j < YTILES; j++)
+                {
+                    //Inizializar la ubicacion del laberinto
+                    mazeTiles[i, j] = new PictureBox();
+
+                    //Posicion del laberinto
+                    int xPosition = (i * TILESIZE) + 13;
+                    int yPosition = (j * TILESIZE) + 45;
+                    mazeTiles[i, j].SetBounds(xPosition, yPosition, TILESIZE, TILESIZE);
+
+                    //Inicio y Final del laberinto
+                    if ((i == 0 && j == 0) || (i == XTILES - 1 && j == YTILES - 1))
+                    {
+                        mazeTiles[i, j].BackColor = Color.Green;
+                    }
+                    else
+                    {
+                        //Lugares en blanco
+                        mazeTiles[i, j].BackColor = Color.White;
+
+                        //Modificar el laberinto
+                        EventHandler clickEvent = new EventHandler(PictureBox_Click);
+                        mazeTiles[i, j].Click += clickEvent;
+                    }
+
+                    //Añadir controles de modificacion
+                    this.Controls.Add(mazeTiles[i, j]);
+                }
+            }
+            
+            //Generar muros
+            for (int i = 0; i < XTILES; i++)
+            {
+                for (int j = 0; j < YTILES; j++)
+                {
+                    if(i % 2 != 0)
+                    {
+                        mazeTiles[i, j].BackColor = Color.Black;
+                    }
+                }
+            }
+            //Generar muros en codigo feo
+            mazeTiles[3, 0].BackColor = Color.White;
+            mazeTiles[7, 0].BackColor = Color.White;
+            mazeTiles[11, 0].BackColor = Color.White;
+            mazeTiles[15, 0].BackColor = Color.White;
+            mazeTiles[19, 0].BackColor = Color.White;
+            mazeTiles[23, 0].BackColor = Color.White;
+            mazeTiles[1, 24].BackColor = Color.White;
+            mazeTiles[5, 24].BackColor = Color.White;
+            mazeTiles[9, 24].BackColor = Color.White;
+            mazeTiles[13, 24].BackColor = Color.White;
+            mazeTiles[17, 24].BackColor = Color.White;
+            mazeTiles[21, 24].BackColor = Color.White;
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+            //Cambiar color a Blanco
+            currentColor = Color.White;
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            //Cambiar color a Negro
+            currentColor = Color.Black;
+        }
+
+        private void PictureBox_Click(object sender, EventArgs e)
+        {
+            //Cambiar el color actual del laberinto
+            ((PictureBox)sender).BackColor = currentColor;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            //Crear lugares por los que ya pasamos
+            bool[,] alreadySearched = new bool[XTILES, YTILES];
+            DateTime inicio = DateTime.Now;
+            //Empieza a resolver de forma recursiva en el inicio
+            if (!solveMaze(0, 0, alreadySearched))
+                MessageBox.Show("Maze can not be solved");
+            else
+            {
+                DateTime final = DateTime.Now;
+                MessageBox.Show("Maze solved in " + (final - inicio));
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Limpia todo el recorrido
+            for (int i = 0; i < XTILES; i++)
+            {
+                for (int j = 0; j < YTILES; j++)
+                {
+                    if (mazeTiles[i, j].BackColor == Color.Blue)
+                        mazeTiles[i, j].BackColor = Color.White;
+                }
+            }
+
+            //Vuelve a colocar el inicio y el final
+            mazeTiles[0, 0].BackColor = Color.Green;
+            mazeTiles[XTILES - 1, YTILES - 1].BackColor = Color.Green;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            bool[,] alreadySearched = new bool[XTILES, YTILES];
+            DateTime inicio = DateTime.Now;
+            //Creacion de hilos
+            Thread Hilo1,Hilo2;
+            if (!hilos)
+            {
+                Hilo1 = new Thread(() => IniciaResuelveHilos1(0, 0, alreadySearched));
+                Hilo2 = new Thread(() => IniciaResuelveHilos2(0, 0, alreadySearched));
+
+                Hilo1.Start();
+                Hilo2.Start();
+
+                DateTime final = DateTime.Now;
+                MessageBox.Show("Maze solved in " + (final - inicio));
+            }
+        }
+
+        private bool solveMaze(int xPos, int yPos, bool[,] alreadySearched)
+        {
+            bool correctPath = false;
+            bool shouldCheck = true;
+
+            //Comprobamos los limites del laberinto
+            if (xPos >= XTILES || xPos < 0 || yPos >= YTILES || yPos < 0)
+                shouldCheck = false;
+            else
+            {
+                //Comprobamos si llego al final
+                if (mazeTiles[xPos, yPos].BackColor == Color.Green && (xPos != 0 && yPos != 0))
+                {
+                    correctPath = true;
+                    shouldCheck = false;
+                }
+
+                //Revisamos los muros
+                if (mazeTiles[xPos, yPos].BackColor == Color.Black)
+                    shouldCheck = false;
+
+                //Revisamos si ya pasamos por ahi
+                if (alreadySearched[xPos, yPos])
+                    shouldCheck = false;
+            }
+
+            //Buscar en el laberinto
+            if (shouldCheck)
+            {
+                //Mostrar camino
+                alreadySearched[xPos, yPos] = true;
+
+                //Movimiento
+                correctPath = correctPath || solveMaze(xPos + 1, yPos, alreadySearched);
+                correctPath = correctPath || solveMaze(xPos, yPos + 1, alreadySearched);
+                correctPath = correctPath || solveMaze(xPos - 1, yPos, alreadySearched);
+                correctPath = correctPath || solveMaze(xPos, yPos - 1, alreadySearched);
+            }
+
+            //Mostrar la ruta
+            if (correctPath)
+                mazeTiles[xPos, yPos].BackColor = Color.Blue;
+
+            return correctPath;
+
+        }
+
+        private bool IniciaResuelveHilos1(int xPos, int yPos, bool[,] alreadySearched)
+        {
+            bool correctPath = false;
+            bool shouldCheck = true;
+
+            if (xPos >= XTILES || xPos < 0 || yPos >= YTILES || yPos < 0)
+            {
+                shouldCheck = false;
+            }
+            else
+            {
+                if (mazeTiles[xPos, yPos].BackColor == Color.Green && (xPos != 0 && yPos != 0))
+                {
+                    correctPath = true;
+                    shouldCheck = false;
+                }
+
+                if (mazeTiles[xPos, yPos].BackColor == Color.Black)
+                {
+                    shouldCheck = false;
+                }
+                if (alreadySearched[xPos, yPos])
+                {
+                    shouldCheck = false;
+                }
+            }
+
+            if (shouldCheck)
+            {
+                alreadySearched[xPos, yPos] = true;
+
+                correctPath = correctPath || IniciaResuelveHilos1(xPos + 1, yPos, alreadySearched);
+                correctPath = correctPath || IniciaResuelveHilos1(xPos, yPos + 1, alreadySearched);
+                correctPath = correctPath || IniciaResuelveHilos1(xPos - 1, yPos, alreadySearched);
+                correctPath = correctPath || IniciaResuelveHilos1(xPos, yPos - 1, alreadySearched);
+            }
+
+            if (correctPath)
+            { 
+                mazeTiles[xPos, yPos].BackColor = Color.Blue;
+            }
+            return correctPath;
+        }
+
+        private bool IniciaResuelveHilos2(int xPos, int yPos, bool[,] alreadySearched)
+        {
+            bool correctPath = false;
+            bool shouldCheck = true;
+
+            if (xPos >= XTILES || xPos < 0 || yPos >= YTILES || yPos < 0)
+            {
+                shouldCheck = false;
+            }
+            else
+            {
+                if (mazeTiles[xPos, yPos].BackColor == Color.Green && (xPos != 0 && yPos != 0))
+                {
+                    correctPath = true;
+                    shouldCheck = false;
+                }
+
+                if (mazeTiles[xPos, yPos].BackColor == Color.Black)
+                {
+                    shouldCheck = false;
+                }
+
+                if (alreadySearched[xPos, yPos])
+                {
+                    shouldCheck = false;
+                }
+            }
+
+            if (shouldCheck)
+            {
+                alreadySearched[xPos, yPos] = true;
+
+                correctPath = correctPath || IniciaResuelveHilos2(xPos, yPos + 1, alreadySearched);
+                correctPath = correctPath || IniciaResuelveHilos2(xPos - 1, yPos, alreadySearched);
+                correctPath = correctPath || IniciaResuelveHilos2(xPos, yPos - 1, alreadySearched);
+                correctPath = correctPath || IniciaResuelveHilos2(xPos + 1, yPos, alreadySearched);
+            }
+
+            if (correctPath)
+            {
+                mazeTiles[xPos, yPos].BackColor = Color.Blue;
+            }
+            return correctPath;
+        }
+    }
+}
